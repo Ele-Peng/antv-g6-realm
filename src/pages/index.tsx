b8f83e600e548238c2f9adc7a79a4164e263a627
pengqian05 <374237925@qq.com>
2020-05-11 14:31:37 +0800

    update: 更新ts写法

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,140 +1,140 @@
 import React from 'react';
-import G6 from "@antv/g6";
+import G6 from '@antv/g6';
 import styles from './index.less';
+import { ChildrenItem } from './data.d';
 
 export default class Index extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-
-    }
-    this.getAllRealmObjects = this.getAllRealmObjects.bind(this)
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+    this.getAllRealmObjects = this.getAllRealmObjects.bind(this);
   }
 
   componentDidMount() {
     // 获取所有的 Global Objects 数据
-    const realmData = this.getAllRealmObjects()
-    this.initChart(realmData)
+    const realmData = this.getAllRealmObjects();
+    this.initChart(realmData);
   }
 
   // 获取所有的 Global Objects 数据
   getAllRealmObjects() {
-    const objects = [
-      "eval",
-      "isFinite",
-      "isNaN",
-      "parseFloat",
-      "parseInt",
-      "decodeURI",
-      "decodeURIComponent",
-      "encodeURI",
-      "encodeURIComponent",
-      "Array",
-      "Date",
-      "RegExp",
-      "Promise",
-      "Proxy",
-      "Map",
-      "WeakMap",
-      "Set",
-      "WeakSet",
-      "Function",
-      "Boolean",
-      "String",
-      "Number",
-      "Symbol",
-      "Object",
-      "Error",
-      "EvalError",
-      "RangeError",
-      "ReferenceError",
-      "SyntaxError",
-      "TypeError",
-      "URIError",
-      "ArrayBuffer",
-      "SharedArrayBuffer",
-      "DataView",
-      "Float32Array",
-      "Float64Array",
-      "Int8Array",
-      "Int16Array",
-      "Int32Array",
-      "Uint8Array",
-      "Uint16Array",
-      "Uint32Array",
-      "Uint8ClampedArray",
-      "Atomics",
-      "JSON",
-      "Math",
-      "Reflect"
+    const objects: string[] = [
+      'eval',
+      'isFinite',
+      'isNaN',
+      'parseFloat',
+      'parseInt',
+      'decodeURI',
+      'decodeURIComponent',
+      'encodeURI',
+      'encodeURIComponent',
+      'Array',
+      'Date',
+      'RegExp',
+      'Promise',
+      'Proxy',
+      'Map',
+      'WeakMap',
+      'Set',
+      'WeakSet',
+      'Function',
+      'Boolean',
+      'String',
+      'Number',
+      'Symbol',
+      'Object',
+      'Error',
+      'EvalError',
+      'RangeError',
+      'ReferenceError',
+      'SyntaxError',
+      'TypeError',
+      'URIError',
+      'ArrayBuffer',
+      'SharedArrayBuffer',
+      'DataView',
+      'Float32Array',
+      'Float64Array',
+      'Int8Array',
+      'Int16Array',
+      'Int32Array',
+      'Uint8Array',
+      'Uint16Array',
+      'Uint32Array',
+      'Uint8ClampedArray',
+      'Atomics',
+      'JSON',
+      'Math',
+      'Reflect',
     ];
 
     const set = new Set();
 
-    const globalObject = {
-      id: "Global Object",
-      children: [
-
-      ]
-    }
+    const globalObject: ChildrenItem = {
+      id: 'Global Object',
+      children: [],
+    };
 
     for (let i of objects) {
       globalObject.children.push({
         children: [],
-        id: i
-      })
+        id: i,
+      });
     }
 
-
     for (let i = 0; i < objects.length; i++) {
-      const current = objects[i]
-      if (set.has(objects[i]))
-        continue;
-      set.add(objects[i])
-      for (let p of Object.getOwnPropertyNames(window[objects[i]])) {
-        let d = Object.getOwnPropertyDescriptor(window[objects[i]], p)
-        if (d.hasOwnProperty("value") && ((d.value !== null && typeof d.value === "object") || (typeof d.value === "function")) && d.value instanceof Object) {
-          let childrenThird = []
+      const current: any = objects[i];
+      if (set.has(objects[i])) continue;
+      set.add(objects[i]);
+      for (let p of Object.getOwnPropertyNames(window[current])) {
+        let d: any = Object.getOwnPropertyDescriptor(window[current], p);
+        if (
+          d.hasOwnProperty('value') &&
+          ((d.value !== null && typeof d.value === 'object') ||
+            typeof d.value === 'function') &&
+          d.value instanceof Object
+        ) {
+          let childrenThird = [];
           for (let k of Object.getOwnPropertyNames(d.value)) {
             if (k !== 'name' && k !== 'length') {
-              childrenThird.push({ id: k })
+              childrenThird.push({ id: k });
             }
           }
-          globalObject["children"][i].children.push({
+          globalObject['children'][i].children.push({
             children: childrenThird,
-            id: p
-          })
+            id: p,
+          });
         }
-        if (d.hasOwnProperty("get") && typeof d.get === "function") {
-          let childrenThird = []
+        if (d.hasOwnProperty('get') && typeof d.get === 'function') {
+          let childrenThird = [];
           for (let k of Object.getOwnPropertyNames(d.get)) {
             if (k !== 'name' && k !== 'length') {
-              childrenThird.push({ id: k })
+              childrenThird.push({ id: k });
             }
           }
-          globalObject["children"][i].children.push({
+          globalObject['children'][i].children.push({
             children: childrenThird,
-            id: p
-          })
+            id: p,
+          });
         }
-        if (d.hasOwnProperty("set") && typeof d.set === "function") {
-          let childrenThird = []
+        if (d.hasOwnProperty('set') && typeof d.set === 'function') {
+          let childrenThird = [];
           for (let k of Object.getOwnPropertyNames(d.set)) {
             if (k !== 'name' && k !== 'length') {
-              childrenThird.push({ id: k })
+              childrenThird.push({ id: k });
             }
           }
-          globalObject["children"][i].children.push({
+          globalObject['children'][i].children.push({
             children: childrenThird,
-            id: p
-          })
+            id: p,
+          });
         }
       }
     }
-    return globalObject
+    return globalObject;
   }
 
-  initChart(data) {
+  initChart(data: any) {
     const width = window.devicePixelRatio * window.screen.width * 0.5;
     const height = window.devicePixelRatio * window.screen.height;
     const graph = new G6.TreeGraph({
@@ -145,7 +145,7 @@ export default class Index extends React.Component {
         default: [
           {
             type: 'collapse-expand',
-            onChange: function onChange(item, collapsed) {
+            onChange: function onChange(item: any, collapsed) {
               const data = item.get('model').data;
               data.collapsed = collapsed;
               return true;
@@ -175,7 +175,7 @@ export default class Index extends React.Component {
       layout: {
         type: 'compactBox',
         direction: 'LR',
-        getId: function getId(d) {
+        getId: function getId(d: ChildrenItem) {
           return d.id;
         },
         getHeight: function getHeight() {
@@ -198,7 +198,8 @@ export default class Index extends React.Component {
         label: node.id,
         labelCfg: {
           offset: 10,
-          position: node.children && node.children.length > 0 ? 'left' : 'right',
+          position:
+            node.children && node.children.length > 0 ? 'left' : 'right',
         },
       };
     });
@@ -208,10 +209,7 @@ export default class Index extends React.Component {
     graph.fitView();
   }
 
-
   render() {
-    return (
-      <div id="container" />
-    );
+    return <div id="container" />;
   }
 }
